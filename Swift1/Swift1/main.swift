77565c46319acbaafdb2b3b87e4d6e4f36cf714b
Stephen Vivash <stephenvivash@hotmail.com>
2019-03-11 22:36:50 +1100

    Swift test

--- Swift1/Swift1/main.swift
+++ Swift1/Swift1/main.swift
@@ -5,16 +5,23 @@ func Test() {
 	
 	Sequences.Initialise()
 	
-	CLineValueSequencer.SetCurrentRatio(nRatio: 5); // 0 - 5 "Equal", "Coin", "Yarrow", "Marbles", "Yin", "Yang"
+	CLineValueSequencer.SetCurrentRatio(nRatio: 1); // 0 - 5 "Equal", "Coin", "Yarrow", "Marbles", "Yin", "Yang"
 	CHexagramValueSequencer.SetCurrentSequence(nSequence: 2); // 0 - 3 "Numeric", "Fuxi", "Wen", "Mystery"
 
-	let ha: CHexagramArray = CHexagramArray()
-	ha.MultiCast(nCount: 10000);
+	var total = 0
+	var coverage = 0
+	let ha = CHexagramArray()
+	let start = Date()
+	ha.MultiCast(count: 5000);
+	let time = abs(Int(start.timeIntervalSinceNow))
 	for h in ha.HexagramArray() {
-		if h.m_nCount > 0 {
-			print("\(String(format: "%4d", h.m_nCount)) \(h.m_strPrimary)");
+		if h.count > 0 {
+			print("\(String(format: "%4d", h.count)) \(h.description)");
+			total += h.count
+			coverage += 1
 		}
 	}
+	print("\nTotal: \(total) Coverage: \(String(format: "%0.2f",Double(coverage) / 40.96))% Time: \(time)s")
 }
 
 Test()
